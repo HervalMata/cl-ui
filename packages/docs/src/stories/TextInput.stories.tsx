8d6a4ea1ed76c7837be45cdc9b4c055b7c98bc1f
HervalMata <hervalmataamparo@gmail.com>
2024-04-26 00:07:16 -0300

    feat:art: design system - create component Text Input
    Signed-off-by: HervalMata <hervalmataamparo@gmail.com>

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,42 @@
+// @ts-ignore
+import {Meta, StoryObj} from '@storybook/react';
+import {TextInput, TextInputProps} from "@cl-ui/react/src/components/TextInput";
+import {Box, Text} from "@cl-ui/react/src";
+
+export default {
+    title: 'Form/Text Input',
+    component: TextInput,
+
+    args: {},
+    decorators: [
+        (Story) => {
+            return (
+                <Box
+                    as="label"
+                    css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+                >
+                    <Text size="sm">Email</Text>
+                    {Story()}
+                </Box>
+            )
+        },
+    ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+    args: {
+        placeholder: 'Type your name',
+    }
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+    args: {
+        prefix: 'cal.com/',
+    },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+    args: {
+        disabled: true,
+    },
+}
