349bc0e388f62fb58ae1bd59ade53daa551fa955
HervalMata <hervalmataamparo@gmail.com>
2024-04-25 23:42:28 -0300

    feat:art: design system - create component Button
    Signed-off-by: HervalMata <hervalmataamparo@gmail.com>

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,6 +1,7 @@
 // @ts-ignore
 import { Button, ButtonProps } from '@cl-ui/react'
 import {Meta, StoryObj} from '@storybook/react';
+import {ArrowRight} from "phosphor-react";
 
 export default {
     title: 'Form/Button',
@@ -13,8 +14,39 @@ export default {
 
 export const Primary: StoryObj<ButtonProps> = {}
 
-export const Big: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
     args: {
-        size: 'small',
+        variant: 'secondary',
+        children: 'Create new',
+    },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+    args: {
+        variant: 'tertiary',
+        children: 'Cancel',
+    },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+    args: {
+        size: 'sm',
+    },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+    args: {
+        children: (
+            <>
+                Próximo passo
+                <ArrowRight weight="bold" />
+            </>
+        ),
+    },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+    args: {
+        disabled: true,
     },
 }
